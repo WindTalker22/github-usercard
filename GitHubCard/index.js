04c852fea21419c5b7502e305461d94bade63d07
MooseMan <ssaciolo@gmail.com>
2019-12-05 15:11:10 -0500

    Created Elements

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,18 @@
            https://api.github.com/users/<your name>
 */
 
+axios.get('https://api.github.com/users/WindTalker22')
+    .then( response => {
+      console.log(response)
+        // Remember response is an object, response.data is an array.
+        // response.data.forEach( item => {
+        //     let button = buttonCreator(item);
+        //     parent.appendChild(button);
+        // })
+    })
+    .catch( error => {
+        console.log("Error:", err);
+    })
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -24,6 +36,7 @@
           user, and adding that card to the DOM.
 */
 
+// 
 const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
@@ -45,7 +58,29 @@ const followersArray = [];
 </div>
 
 */
+function userCard(data) {
+  // Creating Elements
+  const newCard = document.createElement('div'),
+        newImage = document.createElement('img'),
+        newInfo = document.createElement('div'),
+        newName = document.createElement('h3'),
+        newUserName = document.createElement('p'),
+        location = document.createElement('p'),
+        profile = document.createElement('p'),
+        profileLink = document.createElement('a'),
+        followers = document.createElement('p'),
+        following = document.createElement('p'),
+        bio = document.createElement('p');
 
+
+  // Assigning classes 
+  newCard.classList.add()
+
+
+
+  return newCard
+}
+userCard()
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
